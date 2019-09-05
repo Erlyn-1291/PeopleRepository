6e7199818a396145654b608b4159b9fc124c8a89
Centro de Desarrollo De Software <cds@univo.edu.sv>
2019-09-05 08:41:08 -0600

    Models Class Person Added

---
+++ People/People/People/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text; 
+using SQLite;
+
+namespace People.Models
+{
+    [Table("T_People")]
+
+    public class Person
+    {
+        
+        //Miembro Propietes
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
